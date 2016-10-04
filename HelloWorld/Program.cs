a29340a2ed459b3890eadff707a29add6875655f
Polo <Polo@POLO-PC>
2016-10-04 21:42:55 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heeeloo hello");
+            Console.BackgroundColor = ConsoleColor.White;
+        }
+    }99+
+}
